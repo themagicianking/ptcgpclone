cb6cceb322daa78836c67c59b955919db742c649
Thane Chaves <tmoonder@gmail.com>
2025-07-28 21:52:50 -0400

    Initial client setup.

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h1>PTCGP Clone</h1>
+        <p>
+          This app is a WIP clone of the Pokémon Trading Card Game Pocket game.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
